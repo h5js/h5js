4196340967b2b79e32a424c865037ced6861a60d
战哥 <zhanyuan.lizhy@antfin.com>
2017-02-25 10:58:45 +0800

    Array supports

--- src/lib.js
+++ src/lib.js
@@ -29,7 +29,7 @@ var apply;
 
 (function () {
 
-  /** 函数式编程支持：----------------------------------------------------------------- */
+  /** 函数式编程支持：--------------------------------------------------------------- */
   var Function_prototype = Function.prototype;
   var _call = Function_prototype.call;
   var _apply = Function_prototype.apply;
@@ -39,6 +39,67 @@ var apply;
   /* export */ bind = func(_bind);
   /* export */ call = func(_call);
   /* export */ apply = func(_apply);
+
+  /** Array: ---------------------------------------------------------------------- */
+
+  /**
+   * isArray(any)
+   */
+  var isArray = Array.isArray;
+
+  /**
+   * seek(ary, item)
+   *    返回元素item在数组ary中的索引，未找到返回-1;
+   */
+  var seek = func(Array_prototype.indexOf);
+
+  /**
+   * join(ary, separator)
+   *    返回用separator将在数组ary元素连接起来的字符串;
+   */
+  var join = func(Array_prototype.join);
+
+  /**
+   *  unite(ary, ...)
+   */
+  var unite = func(Array_prototype.concat);
+
+  /**
+   *  piece(ary, begin, end)
+   */
+  var piece = func(Array_prototype.slice);
+
+  /**
+   *  splice(ary, idx, len)
+   */
+  var splice = func(Array_prototype.splice);
+
+  /**
+   *  push(ary, item, ...)
+   */
+  var push = func(Array_prototype.push);
+
+  /**
+   *  pop(ary)
+   */
+  var pop = func(Array_prototype.pop);
+
+  /**
+   * peak(ary)
+   */
+  function peak(ary) {
+    return ary[ary.length - 1];
+  }
+
+  /**
+   *  reverse(ary)
+   */
+  var reverse = func(Array_prototype.reverse);
+
+  /**
+   *  map(ary, func)
+   */
+  var map = func(Array_prototype.map);
 
 
 })();
