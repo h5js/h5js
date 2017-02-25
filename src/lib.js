d2455136bce08687cc4ba8e4f22acf85ca677c84
战哥 <zhanyuan.lizhy@antfin.com>
2017-02-25 11:06:58 +0800

    lib.js 特殊，去除局部名字空间

--- src/lib.js
+++ src/lib.js
@@ -2,105 +2,95 @@
  *
  */
 
+
+/** 函数式编程支持：--------------------------------------------------------------- */
+var Function_prototype = Function.prototype;
+var _call = Function_prototype.call;
+var _apply = Function_prototype.apply;
+var _bind = Function_prototype.bind;
+
 /**
  * func(method)
  *    将方法转换为函数。若fn = func(method), 则 fn(obj, ...) 等价于 obj.method(...)
  */
-var func;
+var func = _call.bind(_bind, _call);
 
 /**
  * bind(func, this, ...)
  *    创建函数 func 的绑定函数.
  */
-var bind;
+var bind = func(_bind);
 
 /**
  * call(func, thisArg, ...)
  *    调用函数 func, 一般参数形式.
  */
-var call;
+var call = func(_call);
 
 /**
  * apply(func, thisArg, [...])
  *    调用函数 func, 数组参数形式.
  */
-var apply;
+var apply = func(_apply);
 
+/** Array: ---------------------------------------------------------------------- */
 
-(function () {
+/**
+ * isArray(any)
+ */
+var isArray = Array.isArray;
 
-  /** 函数式编程支持：--------------------------------------------------------------- */
-  var Function_prototype = Function.prototype;
-  var _call = Function_prototype.call;
-  var _apply = Function_prototype.apply;
-  var _bind = Function_prototype.bind;
+/**
+ * seek(ary, item)
+ *    返回元素item在数组ary中的索引，未找到返回-1;
+ */
+var seek = func(Array_prototype.indexOf);
 
-  /* export */ func = _call.bind(_bind, _call);
-  /* export */ bind = func(_bind);
-  /* export */ call = func(_call);
-  /* export */ apply = func(_apply);
+/**
+ * join(ary, separator)
+ *    返回用separator将在数组ary元素连接起来的字符串;
+ */
+var join = func(Array_prototype.join);
 
-  /** Array: ---------------------------------------------------------------------- */
+/**
+ *  unite(ary, ...)
+ */
+var unite = func(Array_prototype.concat);
 
-  /**
-   * isArray(any)
-   */
-  var isArray = Array.isArray;
+/**
+ *  piece(ary, begin, end)
+ */
+var piece = func(Array_prototype.slice);
 
-  /**
-   * seek(ary, item)
-   *    返回元素item在数组ary中的索引，未找到返回-1;
-   */
-  var seek = func(Array_prototype.indexOf);
+/**
+ *  splice(ary, idx, len)
+ */
+var splice = func(Array_prototype.splice);
 
-  /**
-   * join(ary, separator)
-   *    返回用separator将在数组ary元素连接起来的字符串;
-   */
-  var join = func(Array_prototype.join);
+/**
+ *  push(ary, item, ...)
+ */
+var push = func(Array_prototype.push);
 
-  /**
-   *  unite(ary, ...)
-   */
-  var unite = func(Array_prototype.concat);
+/**
+ *  pop(ary)
+ */
+var pop = func(Array_prototype.pop);
 
-  /**
-   *  piece(ary, begin, end)
-   */
-  var piece = func(Array_prototype.slice);
+/**
+ * peak(ary)
+ */
+function peak(ary) {
+  return ary[ary.length - 1];
+}
 
-  /**
-   *  splice(ary, idx, len)
-   */
-  var splice = func(Array_prototype.splice);
+/**
+ *  reverse(ary)
+ */
+var reverse = func(Array_prototype.reverse);
 
-  /**
-   *  push(ary, item, ...)
-   */
-  var push = func(Array_prototype.push);
-
-  /**
-   *  pop(ary)
-   */
-  var pop = func(Array_prototype.pop);
-
-  /**
-   * peak(ary)
-   */
-  function peak(ary) {
-    return ary[ary.length - 1];
-  }
-
-  /**
-   *  reverse(ary)
-   */
-  var reverse = func(Array_prototype.reverse);
-
-  /**
-   *  map(ary, func)
-   */
-  var map = func(Array_prototype.map);
-
-
-})();
+/**
+ *  map(ary, func)
+ */
+var map = func(Array_prototype.map);
 
