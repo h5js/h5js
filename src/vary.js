183d511ebf04eef6e817383803d38851851f98be
战哥 <zhanyuan.lizhy@antfin.com>
2017-03-12 15:53:19 +0800

    重构 vary.js

--- src/vary.js
+++ src/vary.js
@@ -3,135 +3,97 @@
  */
 
 /**
- *  attachValueVary(at, it, to, me)
+ *  attachVary(it, on, me)
  */
-var attachValueVary;
+var attachVary;
 
 /**
- * detachValueVary(at, it, to, me)
+ * detachVary(it, on, me)
  */
-var detachValueVary;
-
-/**
- * attachArrayVary(it, to, me)
- */
-var attachArrayVary;
-
-/**
- * detachArrayVary(it, to, me);
- */
-var detachArrayVary;
+var detachVary;
 
 (function () {
-  var valueVaries = [];
-  var valueIdx = 0;
+  var varies = [];
+  var index = 0;
+  var interval;
 
-  attachValueVary = function (at, it, to, me) {
-    if((isFunction(at) || isObject(it)) && (isFunction(to) || isObject(me))) {
-      for (var i = valueVaries.length, vary; (vary = valueVaries[--i]) && (vary[0] !== at || vary[1] !== it || vary[2] !== to || vary[3] !== me););
+  attachVary = function (it, on, me) {
+    if((isObject(it)) && (isFunction(on) || isObject(me))) {
+      var length, i, vary;
+      for (length = i = varies.length; (vary = varies[--i]) && (vary[0] !== it || vary[1] !== on || vary[2] !== me););
       if (i < 0) {
-        push(valueVaries, [at, it, to, me]);
+        varies[length] = [it, on, me, []];
+        if(!length) {
+          interval = setInterval(digestVaries, 0);
+        }
       }
     }
   };
 
-  detachValueVary = function (at, it, to, me) {
-    for (var i = valueVaries.length, vary; (vary = valueVaries[--i]) && (vary[0] !== at || vary[1] !== it || vary[2] !== to || vary[3] !== me););
-    if (i >= 0) {
-      splice(valueVaries, i, 1);
-      if (i <= valueIdx) {
-        valueIdx--;
+  detachVary = function (it, on, me) {
+    if((isObject(it)) && (isFunction(on) || isObject(me))) {
+      var length, i, vary;
+      for (length = i = varies.length; (vary = varies[--i]) && (vary[0] !== it || vary[1] !== on || vary[2] !== me););
+      if (i >= 0) {
+        splice(varies, i, 1);
+        if(length === 1) {
+          clearInterval(interval);
+        }
+        if (i <= index) {
+          index--;
+        }
       }
     }
   };
 
-  function digestValueVaries() {
+  function digestVaries() {
     var vary;
-    for (valueIdx = 0; vary = valueVaries[valueIdx]; valueIdx++) {
-      var oldValue = vary[4], newValue;
-      var at = vary[0], it = vary[1];
+    for (index = 0; vary = varies[index]; index++) {
+      var it = vary[0], on = vary[1], me = vary[2], at = vary[3];
 
-      if (isFunction(at)) {
-        newValue = call(at, it);
+      if (isFunction(it)) {
+        it = it();
+        if(it !== at) {
+          if(isFunction(on)) {
+            call(on, me, it);
+          }
+          else {
+            me[on] = it;
+          }
+          vary[3] = it;
+        }
       }
       else {
-        newValue = it[at];
-      }
-
-      if (oldValue !== newValue) {
-        var to = vary[2], me = vary[3];
-        if (isFunction(to)) {
-          call(to, me, newValue);
+        var varied, idx = 0, pos;
+        while(idx < at.length && idx < it.length) {
+          var atValue = at[idx], itValue = it[idx];
+          if(atValue === itValue) {
+            if(varied)
+              call(on, me, 0, idx, idx);
+          }
+          else if((pos = seek(at, itValue, idx+1)) < 0) {
+            splice(at, idx, 0, itValue);
+            call(on, me, 1, idx, itValue);
+            varied = 1;
+          }
+          else {
+            splice(at, idx, 0, splice(at, pos, 1)[0]);
+            call(on, me, 0, pos, idx);
+            varied = 1;
+          }
+          idx ++;
         }
-        else {
-          me[to] = newValue;
+        pos = at.length;
+        if(idx < pos) {
+          pos -= idx;
+          splice(at, idx, pos);
+          call(on, me, -1, idx, pos);
         }
-        vary[4] = newValue;
+        else for(pos=it.length; idx<pos; idx++){
+          call(on, me, 1, idx, at[idx] = it[idx]);
+        }
       }
     }
   }
-
-
-  var arrayVaries = [];
-  var arrayIdx = 0;
-
-  attachArrayVary = function(it, to, me) {
-    if(isObject(it) && isFunction(to)) {
-      for (var i = arrayVaries.length, vary; (vary = arrayVaries[--i]) && (vary[0] !== it || vary[1] !== to || vary[2] !== me););
-      if (i < 0) {
-        push(arrayVaries, [it, to, me, bind(to, me), piece(it)]);
-      }
-    }
-  };
-
-  detachArrayVary = function(it, to, me) {
-    for (var i = arrayVaries.length, vary; (vary = arrayVaries[--i]) && (vary[0] !== it || vary[1] !== to || vary[2] !== me););
-    if (i >= 0) {
-      splice(arrayVaries, i, 1);
-      if (i <= arrayIdx) {
-        arrayIdx--;
-      }
-    }
-  };
-
-  function digestArrayVaries() {
-    var vary;
-    for (arrayIdx = 0; vary = arrayVaries[arrayIdx]; arrayIdx++) {
-      var newArray = vary[0], oldArray = vary[4], notify = vary[3], varied, newIdx = 0, oldIdx;
-
-      while (newIdx < oldArray.length && newIdx < newArray.length) {
-        var oldValue = oldArray[newIdx];
-        var newValue = newArray[newIdx];
-        if (oldValue === newValue) {
-          if(varied)
-            notify(0, newIdx, newIdx);
-        }
-        else if ((oldIdx = seek(oldArray, newValue, newIdx + 1)) < 0) {
-          splice(oldArray, newIdx, 0, newValue);
-          notify(1, newIdx, newValue);
-          varied = 1;
-        }
-        else {
-          splice(oldArray, newIdx, 0, splice(oldArray, oldIdx, 1)[0]);
-          notify(0, oldIdx, newIdx);
-          varied = 1;
-        }
-        newIdx++;
-      }
-      oldIdx = oldArray.length;
-      if(newIdx < oldIdx) {
-        splice(oldArray, newIdx, oldIdx - newIdx);
-        notify(-1, newIdx, oldIdx - newIdx);
-      }
-      else for(oldIdx = newArray.length; newIdx < oldIdx; newIdx++) {
-        notify(1, newIdx, oldArray[newIdx] = newArray[newIdx]);
-      }
-    }
-  }
-
-  setInterval(function(){
-    digestValueVaries();
-    digestArrayVaries();
-  }, 0);
 
 })();
