8ef9c2ffb6a1bc8a6fff8064d6c1314c1ebff986
战哥 <zhanyuan.lizhy@antfin.com>
2017-03-22 09:46:49 +0800

    merge vary.js from com branch

--- src/vary.js
+++ src/vary.js
@@ -13,87 +13,87 @@ var attachVary;
 var detachVary;
 
 (function () {
-  var varies = [];
-  var index = 0;
-  var interval;
+    var varies = [];
+    var index = 0;
+    var interval;
 
-  attachVary = function (it, on, me) {
-    if(isObject(it) && (isFunction(on) || isObject(me))) {
-      var length, i, vary;
-      for (length = i = varies.length; (vary = varies[--i]) && (vary[0] !== it || vary[1] !== on || vary[2] !== me););
-      if (i < 0) {
-        varies[length] = [it, on, me, []];
-        if(!length) {
-          interval = setInterval(digestVaries, 0);
+    attachVary = function (it, on, me) {
+        if(isObject(it) && (isFunction(on) || isObject(me))) {
+            var length, i, vary;
+            for (length = i = varies.length; (vary = varies[--i]) && (vary[0] !== it || vary[1] !== on || vary[2] !== me););
+            if (i < 0) {
+                varies[length] = [it, on, me, []];
+                if(!length) {
+                    interval = setInterval(digestVaries, 0);
+                }
+            }
         }
-      }
+    };
+
+    detachVary = function (it, on, me) {
+        if(isObject(it) && (isFunction(on) || isObject(me))) {
+            var length, i, vary;
+            for (length = i = varies.length; (vary = varies[--i]) && (vary[0] !== it || vary[1] !== on || vary[2] !== me););
+            if (i >= 0) {
+                splice(varies, i, 1);
+                if(length === 1) {
+                    clearInterval(interval);
+                }
+                if (i <= index) {
+                    index--;
+                }
+            }
+        }
+    };
+
+    function digestVaries() {
+        var vary;
+        for (index = 0; vary = varies[index]; index++) {
+            var it = vary[0], on = vary[1], me = vary[2], at = vary[3];
+
+            if (isFunction(it)) {
+                it = it();
+                if(it !== at) {
+                    vary[3] = it;
+                    if(isFunction(on)) {
+                        call(on, me, it);
+                    }
+                    else {
+                        me[on] = it;
+                    }
+                }
+            }
+            else {
+                var varied, idx = 0, pos;
+                while(idx < at.length && idx < it.length) {
+                    var atValue = at[idx], itValue = it[idx];
+                    if(atValue === itValue) {
+                        if(varied)
+                            call(on, me, 0, idx, idx);
+                    }
+                    else if((pos = seek(at, itValue, idx+1)) < 0) {
+                        splice(at, idx, 0, itValue);
+                        call(on, me, 1, idx, itValue);
+                        varied = 1;
+                    }
+                    else {
+                        splice(at, idx, 0, splice(at, pos, 1)[0]);
+                        call(on, me, 0, pos, idx);
+                        varied = 1;
+                    }
+                    idx ++;
+                }
+                pos = at.length;
+                if(idx < pos) {
+                    pos -= idx;
+                    splice(at, idx, pos);
+                    call(on, me, -1, idx, pos);
+                }
+                else for(pos=it.length; idx<pos; idx++){
+                    call(on, me, 1, idx, at[idx] = it[idx]);
+                }
+            }
+        }
     }
-  };
-
-  detachVary = function (it, on, me) {
-    if(isObject(it) && (isFunction(on) || isObject(me))) {
-      var length, i, vary;
-      for (length = i = varies.length; (vary = varies[--i]) && (vary[0] !== it || vary[1] !== on || vary[2] !== me););
-      if (i >= 0) {
-        splice(varies, i, 1);
-        if(length === 1) {
-          clearInterval(interval);
-        }
-        if (i <= index) {
-          index--;
-        }
-      }
-    }
-  };
-
-  function digestVaries() {
-    var vary;
-    for (index = 0; vary = varies[index]; index++) {
-      var it = vary[0], on = vary[1], me = vary[2], at = vary[3];
-
-      if (isFunction(it)) {
-        it = it();
-        if(it !== at) {
-          if(isFunction(on)) {
-            call(on, me, it);
-          }
-          else {
-            me[on] = it;
-          }
-          vary[3] = it;
-        }
-      }
-      else {
-        var varied, idx = 0, pos;
-        while(idx < at.length && idx < it.length) {
-          var atValue = at[idx], itValue = it[idx];
-          if(atValue === itValue) {
-            if(varied)
-              call(on, me, 0, idx, idx);
-          }
-          else if((pos = seek(at, itValue, idx+1)) < 0) {
-            splice(at, idx, 0, itValue);
-            call(on, me, 1, idx, itValue);
-            varied = 1;
-          }
-          else {
-            splice(at, idx, 0, splice(at, pos, 1)[0]);
-            call(on, me, 0, pos, idx);
-            varied = 1;
-          }
-          idx ++;
-        }
-        pos = at.length;
-        if(idx < pos) {
-          pos -= idx;
-          splice(at, idx, pos);
-          call(on, me, -1, idx, pos);
-        }
-        else for(pos=it.length; idx<pos; idx++){
-          call(on, me, 1, idx, at[idx] = it[idx]);
-        }
-      }
-    }
-  }
 
 })();
